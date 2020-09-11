2c49bea468d4559fcc57accaed181132024513ee
Appscom <60970424+Appscom@users.noreply.github.com>
2020-09-11 17:27:30 +0530

    Update __manifest__.py

--- stock_picking_auto_create_lot/__manifest__.py
+++ stock_picking_auto_create_lot/__manifest__.py
@@ -6,11 +6,8 @@
     "version": "13.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "stock",
-    "website": "https://github.com/OCA/stock-logistics-workflow",
-    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": ["stock"],
     "data": ["views/product_views.xml", "views/stock_picking_type_views.xml"],
-    "maintainers": ["sergio-teruel"],
 }
