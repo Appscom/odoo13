33142f2da18d390be9ea9006a9de7436b66fd471
Appscom <60970424+Appscom@users.noreply.github.com>
2020-09-11 17:23:11 +0530

    Update __manifest__.py

--- bt_sales_warranty/__manifest__.py
+++ bt_sales_warranty/__manifest__.py
@@ -1,34 +1,16 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2017-BroadTech IT Solutions (<http://www.broadtech-innovations.com/>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##############################################################################
+
 
 {
     'name': 'Sales Warranty',
     'version': '0.1',
-    'author' : 'BroadTech IT Solutions Pvt Ltd',
     'category': 'Sales',
     'license':'AGPL-3',
     'summary' : 'Sales Warranty ',
     'description': """
 The module helps to manage the warranty details and service information of products sold to customers and thus smoothen the after sales activities.
     """,
-    'website': 'http://www.broadtech-innovations.com',
+    #'website': 'http://www.broadtech-innovations.com',
     'depends': ['sale'],
     'data': [
         'data/warranty_name_sequence.xml',
@@ -41,7 +23,7 @@ The module helps to manage the warranty details and service information of produ
         'report/warranty_report.xml',
         'report/warranty_detail_report.xml'
     ],
-    'images': ['static/description/bt_sales_warranty_banner.jpg'],
+    #'images': ['static/description/bt_sales_warranty_banner.jpg'],
     'installable': True,
     'auto_install': False,
 }
