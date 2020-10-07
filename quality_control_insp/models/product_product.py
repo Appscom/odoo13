2a40aa01843f0414892b90e72e7bc6302ba1ac61
Appscom <60970424+Appscom@users.noreply.github.com>
2020-10-07 16:56:08 +0530

    Update product_product.py

--- quality_control_insp/models/product_product.py
+++ quality_control_insp/models/product_product.py
@@ -12,5 +12,4 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     qc_triggers = fields.One2many(
-        comodel_name="qc.trigger.product_line", inverse_name="product", 
-        string="Quality control triggers")
+        string="Quality control triggers",related ="product_tmpl_id.qc_triggers")
